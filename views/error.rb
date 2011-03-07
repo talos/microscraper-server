3c79b7febf7fc9f730999e1c568a6794524ad979
talos <irving.krauss@gmail.com>
2011-03-06 22:37:26 -0500

    multiple editors works, starting on scraper view

--- views/error.rb
+++ views/error.rb
@@ -1,7 +1,7 @@
 module SimpleScraper
   class Application
     module Views
-      class Error < Mustache
+      class Error < Layout
         def errors
           env['sinatra.error'].inspect or response
         end
