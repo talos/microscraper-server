6a96d2d3889bb253026ec440c117c1f08835d483
talos <irving.krauss@gmail.com>
2011-04-06 18:57:56 -0400

    commenting out prior changes until they can be reevaluated

--- lib/dm-helpers.rb
+++ lib/dm-helpers.rb
@@ -15,65 +15,65 @@ module DataMapper
     end
   end
 
-  module Resource
-    def relationships
-      one_to_manys = model.relationships.select do |name, relationship|
-        relationship.class == DataMapper::Associations::OneToMany::Relationship
-      end
+  # module Resource
+  #   def relationships
+  #     one_to_manys = model.relationships.select do |name, relationship|
+  #       relationship.class == DataMapper::Associations::OneToMany::Relationship
+  #     end
 
-      one_to_manys.collect do |name, relationship|
-        {
-          :relationship => relationship,
-          :links => send(name).collect do |resource|
+  #     one_to_manys.collect do |name, relationship|
+  #       {
+  #         :relationship => relationship,
+  #         :links => send(name).collect do |resource|
             
-          end
-        }
-      end
-    end
+  #         end
+  #       }
+  #     end
+  #   end
     
-    def is_resource?
-      respond_to? :full_name
-    end
+  #   def is_resource?
+  #     respond_to? :full_name
+  #   end
 
-    def is_link?
-      key.length == 2 and model.relationships.size == 2
-    end
+  #   def is_link?
+  #     key.length == 2 and model.relationships.size == 2
+  #   end
 
-    # TODO LINK SPECIFIC
-    def source
-      send(model.relationships.to_a[0][0])
-    end
+  #   # TODO LINK SPECIFIC
+  #   def source
+  #     send(model.relationships.to_a[0][0])
+  #   end
 
-    def target
-      send(model.relationships.to_a[1][0])
-    end
+  #   def target
+  #     send(model.relationships.to_a[1][0])
+  #   end
 
-    def location
-      # this is a resource
-      if is_resource?
-        model.location + full_name
-        # this is a link
-      elsif is_link?
-        # Track down the many-to-many relationship that uses this link.
-        relationship = source.model.relationships.find do |name, relationship|
-          if relationship.respond_to? :through
-            relationship.through.target_model == model
-          end
-        end
-        # The location of one of the linked resources, plus the key of the second.
-        if relationship
-          source.location + '/' + relationship[0].to_s + '/' + target.key.to_s
-        end
-      end
-    end
+  #   def location
+  #     # this is a resource
+  #     if is_resource?
+  #       model.location + full_name
+  #       # this is a link
+  #     elsif is_link?
+  #       # Track down the many-to-many relationship that uses this link.
+  #       relationship = source.model.relationships.find do |name, relationship|
+  #         if relationship.respond_to? :through
+  #           relationship.through.target_model == model
+  #         end
+  #       end
+  #       # The location of one of the linked resources, plus the key of the second.
+  #       if relationship
+  #         source.location + '/' + relationship[0].to_s + '/' + target.key.to_s
+  #       end
+  #     end
+  #   end
 
-    def follow(from_resource)
-      if is_resource?
-        return self
-      elsif is_link?
+  #   def follow(from_resource)
+  #     if is_resource?
+  #       return self
+  #     elsif is_link?
         
-      end
-    end
-  end
+  #     end
+  #   end
+  # end
 end
 # require 'lib/database'; db = MicroScraper::Database.new; user = db.user_model.first; scraper = user.scrapers.first; scraper.web_pages.intermediaries.first.location
