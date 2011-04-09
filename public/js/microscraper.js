66e524a7c219bdfd9bb9fe8ab6ac2e3bb56b5191
talos <irving.krauss@gmail.com>
2011-04-09 19:18:24 -0400

    working applet interactions

--- public/js/microscraper.js
+++ public/js/microscraper.js
@@ -5,6 +5,11 @@
 **/
 
 $(document).ready(function() {
+    if(!$('body').data('microscraper')) {
+	$('body').data('microscraper', {});
+    }
+    var data = $('body').data('microscraper');
+
     /* Allows for PUT and DELETE from forms, reloads upon success. */
     $('form.ajax').each(function() {
 	var method = $(this).attr('method');
@@ -18,54 +23,14 @@ $(document).ready(function() {
 	    }
 	});
     });
+    
     /* Testing. Intercept test form submission and give it to the applet. */
-    $('form.test').submit(function(event) {
-	var $form = $(this),
-	json_location = $form.attr('action'),
-	data = $('body').data('microscraper');
-	
-	$form.spinner( { img : data.spinner_img } );
-	
-	var defaults = $form.serializeArray();
-	    
-	try {
-	    /* Create the applet if this is the user's first click to test. */
-	    if(!data.applet) {
-		var $applet = $('<applet>').attr({
-		    code : data.applet_class,
-		    archive : data.applet_jar,
-		    codebase : data.applet_dir,
-		    width : "0",
-		    height : "0"
-		});
-		$form.append($applet);
-		data.applet = $form.children('applet').get(0);
-	    }
-	    
-	    /* The applet will throw TypeErrors until it's ready. */
-	    var tryScraper = function() {
-		try {
-		    data.applet.scrape(json_location, defaults);
-		    $form.spinner('remove');
-		} catch(error) {
-		    if(error instanceof TypeError) {
-			setTimeout(tryScraper, 100);
-		    } else {
-			$form.spinner('remove');
-			// $.error(error);
-			throw error;
-		    }
-		}
-	    }
-	    tryScraper();
-	    
-	    return false;
-	} catch(error) {
-	    $form.spinner('remove');
-	    event.preventDefault();
-	    $.error(error);
-	}
+    $('form.test').microscraper_applet({
+	code : data.applet_class,
+	archive : data.applet_jar,
+	codebase : data.applet_dir
     });
+
     /* Autofill 'add' inputs, which are used for tagging & resource creation. */
     $('input.add').each(function() {
 	var $input = $(this);
