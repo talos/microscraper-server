75d79eba519b8314f801472323fb02d44d5185d6
Talos <irving.krauss@gmail.com>
2011-05-09 16:04:00 -0400

    working on tests

--- lib/resources/mixins/locatable.rb
+++ lib/resources/mixins/locatable.rb
@@ -11,24 +11,20 @@ module MicroScraper
     # The URI is generated using #{DEFAULT_MODEL} and #{DEFAULT_RESOURCE}
     module Locatable
       
-      # Default URI root for Locatable models & resources.
-      DEFAULT_ROOT = Addressable::URI.parse('/')
-      
       # Default URI template for a Locatable model.
       DEFAULT_MODEL = Addressable::Template.new('{model}/')
-
+      
       # Default URI template for a Locatable resource (appended to the model)
-      DEFAULT_RESOURCE = Addressable::Template.new('{-suffix|/|within}{title}')
+      DEFAULT_RESOURCE = Addressable::Template.new('{model}/{-suffix|/|within}{title}')
       
       # This class holds all the necessary templates for Locatables.
-      class LocatableTemplate
-        # @param [Hash] opts options to create the LocatableTemplate
-        # @option opts [Addressable::URI]      :root     Root URI
-        # @option opts [Addressable::Template] :model    Model template (combined with Root URI)
-        # @option opts [Addressable::Template] :resource Resource template (combined with Model template)
+      class Template
+        # @param [Hash] opts options to create the Locatable::Template
+        # @option opts [Addressable::Template] :model    Model template
+        # @option opts [Addressable::Template] :resource Resource template
         def initialize(opts)
-          @model =    Addressable::Template.new(opts[:root].to_s + opts[:model].pattern)
-          @resource = Addressable::Template.new(@model.pattern + opts[:resource].pattern)
+          @model =    opts[:model]
+          @resource = opts[:resource]
         end
 
         # @param [Addressable::URI] uri URI to extract from
@@ -65,17 +61,23 @@ module MicroScraper
       end
 
       # Modify the template used by Locatable
-      # @param [MicroScraper::Resources::Locatable::LocatableTemplate] template a new template
+      # @param [MicroScraper::Resources::Locatable::Template] template a new template
       # @return [MicroScraper::Resources::Locatable] the module
       def self.set_template(template)
         @@template = template
         self
       end
+
+      # Get the template currently used by Locatable
+      # @return [MicroScraper::Resources::Locatable::Template] the current template
+      def self.get_template
+        @@template
+      end
       
       # A hash of all locatable classes, keyed by their location.
       @@locatables = Hash.new
       
-      @@template = LocatableTemplate.new ({:root => DEFAULT_ROOT, :model => DEFAULT_MODEL, :resource => DEFAULT_RESOURCE })
+      @@template = Template.new ({ :model => DEFAULT_MODEL, :resource => DEFAULT_RESOURCE })
       
       def self.included(base)
         base.class_eval do
@@ -116,36 +118,32 @@ module MicroScraper
 
 
       # Obtain an absolute path the resource.
-      # @param [MicroScraper::Resources::Locatable::LocatableTemplate] template Optional alternate template.
       # @return [Addressable::URI] an absolute path to the resource.
-      def location(template = @@template)
+      def location
         if @within_relationship
-          template.expand_resource(self.model, self.title, send(@within_relationship).attribute_get(@within_property))
+          @@template.expand_resource(self.model, self.title, send(@within_relationship).attribute_get(@within_property))
         else
-          template.expand_resource(self.model, self.title)
+          @@template.expand_resource(self.model, self.title)
         end
       end
       
       # Obtain a Locatable resource model from a URI.
       # @param [Addressable::URI] uri URI for model
-      # @param template (see #location)
       # @return [DataMapper::Model] a DataMapper model with the Locatable Mixin
-      def self.get_model(uri, template = @@template)
-        @@locatables[template.extract_model_name(uri)]
+      def self.get_model(uri)
+        @@locatables[@@template.extract_model_name(uri)]
       end
       
       # Obtain a resource based off of a URI
       # @param [Addressable::URI] uri URI for resource
-      # @param template (see #location)
       # @return [DataMapper::Resource] a DataMapper resource with the Locatable Mixin
-      def self.get_resource(uri, template = @@template)
-        model = get_model(template)
+      def self.get_resource(uri)
         within = template.extract_within(uri)
         title = template.extract_title(uri)
         if within
-          model.first(@within_relationship => { @within_property => within }, :title => title)
+          get_model.first(@within_relationship => { @within_property => within }, :title => title)
         else
-          model.first(:title => title)
+          get_model.first(:title => title)
         end
       end
     end
--- spec/resources/mixins/locatable.rb
+++ spec/resources/mixins/locatable.rb
@@ -1,20 +1,65 @@
 require 'rubygems'
 require 'rspec'
 require 'addressable/uri'
+require 'addressable/template'
 
 require 'lib/resources/mixins/locatable'
 
-describe MicroScraper::Resources::Locatable, '#uri' do
-  class TestModel
-    include MicroScraper::Resources::Locatable
-  end
-  
-  it 'provides uri for model' do
-    TestModel.location.should == Addressable::URI.parse('/TestModel/')
-  end
+module MicroScraper::Resources
+  describe Locatable do
+    let!(:default_template) do
+      Locatable.get_template
+    end
 
-  it 'provides uri for resource' do
-    resource = TestModel.new(:title => 'title')
-    resource.location.should == Addressable::URI.parse('/TestModel/title')
+    let!(:alternate_template) do
+      Locatable::Template.new({
+                              :root => Addressable::URI.parse('/alternate/'),
+                              :model => Addressable::Template.new('models/{model}/'),
+                              :resource => Addressable::Template.new('{-suffix|-|within}{title}') 
+                            })
+    end
+
+    let!(:test_module) do
+      Locatable
+    end
+
+    let!(:test_model) do
+      LocatableModel = Class.new do
+        include 
+      end
+    end
+
+    let!(:test_resource) do
+      test_model.new
+    end
+    
+    puts test_module
+    describe test_module do
+      it 'should have a default template' do
+        puts test_module
+        test_module.get_template.class.should == alternate_template.class
+      end
+      
+      it 'should change templates' do
+        test_module.set_template(alternate_template)
+        test_module.get_template.should == alternate_template
+        test_module.get_template.should != default_template
+        test_module.set_template(default_template)
+        test_module.get_template.should != alternate_template
+        test_module.get_template.should == default_template
+      end
+    end
+
+    describe test_model, '#location' do    
+      it 'generates uri with default template' do
+        test_model.location.should == Addressable::URI.parse('/TestLocatable/')
+      end
+    end
+
+    describe test_resource, '#location' do
+      it 'generates uri with default template' do
+        test_resource.location.should == Addressable::URI.parse('/TestLocatable/title')
+      end
+    end
   end
 end
